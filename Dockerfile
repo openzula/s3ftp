451aef3ca42c131883ffe92da27a07f9c55a6a66
Alex Cartwright <alex@openzula.org>
2020-04-06 09:49:00 +0100

    Mount an AWS S3 bucket at the FTP users home directory (s3fs)
    The entire home directory becomes an S3 bucket which you can then access
    via FTP (the entire purpose of this Docker image).
    
    The following environmental variables are available:
    
    * FTP_S3_ACCESS_KEY - AWS IAM access key
    * FTP_S3_SECRET - AWS IAM secret
    * FTP_S3_BUCKET - The name of the AWS S3 bucket to mount
    * FTP_S3_REGION - (optional, default 'eu-west-2')

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ LABEL maintainer="alex@openzula.org"
 EXPOSE 20 21 10090-10100
 
 RUN apk update
+RUN apk add --no-cache -X http://dl-cdn.alpinelinux.org/alpine/edge/testing s3fs-fuse
 RUN apk add --no-cache vsftpd
 
 COPY vsftpd.conf /etc/vsftpd/vsftpd.conf
--- provision.sh
+++ provision.sh
@@ -4,6 +4,7 @@
 ## will chroot the user to the parent directory ("/media/ftp"), but
 ## upon login will change directory to "/media/ftp/$user"
 FTP_USER_HOME="/media/ftp/./$FTP_USER"
+FTP_DATA_DIR="$FTP_USER_HOME"
 
 adduser -h "$FTP_USER_HOME" -s /bin/false -D "$FTP_USER"
 chown -R "$FTP_USER":"$FTP_USER" "$FTP_USER_HOME"
@@ -14,5 +15,14 @@ if [ -z "$FTP_PWD" ]; then
 fi
 
 echo "${FTP_USER}:${FTP_PWD}" | /usr/sbin/chpasswd 2> /dev/null
+
+## Configure AWS S3 Bucket mount
+echo "${FTP_S3_ACCESS_KEY}:${FTP_S3_SECRET}" > /etc/passwd-s3fs
+chmod 0400 /etc/passwd-s3fs
+
+s3fs "$FTP_S3_BUCKET" "$FTP_DATA_DIR" \
+    -o allow_other,umask=077,uid="$(id -u "$FTP_USER")",gid="$(id -g "$FTP_USER")" \
+    -o passwd_file=/etc/passwd-s3fs \
+    -o url="https://s3-${FTP_S3_REGION:-eu-west-2}.amazonaws.com"
 
 /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
